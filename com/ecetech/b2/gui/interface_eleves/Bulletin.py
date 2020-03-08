e08a3ebcb306e15ee700b8ff02a8b1d79154655f
DAGORNE <47077580+DamsRock@users.noreply.github.com>
2020-03-08 16:53:00 +0100

    upd bulletin

--- com/ecetech/b2/gui/interface_eleves/Bulletin.py
+++ com/ecetech/b2/gui/interface_eleves/Bulletin.py
@@ -1,23 +1,22 @@
-from tkinter import *
-import lxml.etree as et
+import requests
+import tkinter as tk
 import xml.etree.ElementTree as ET
 
 # Création de la fenêtre principale
-root = Tk()
+root = tk.Tk()
 root.title('Bulletin')
 root.geometry('800x150')
 
 # Fonction de Séparation des modules
 def separation(r, c, h, w):
-    ligne = Text(root, height=h, width=w)
+    ligne = tk.Label(root, height=h, width=w)
     ligne.configure({"background": "black"})
     ligne.grid(row=r, column=c)
 
 
 # Fonction création entré
-def entre(r, c, h, w, nom):
-    champ = Text(root, height=h, width=w)
-    champ.insert(INSERT, nom)
+def entre(r, c, h, w, la):
+    champ = tk.Label(root, text=str(la), height=h, width=w)
     champ.grid(row=r, column=c)
 
 
@@ -29,38 +28,41 @@ cvmw = 17  # largeur champs validation module
 crw = 10  # largeur champs rattrapage
 cECTSw = 4  # largeur champs ECTS
 
-# Création et remplissage des différentes catégories
-entre(0, 0, 1, cmw, 'Matières')
-entre(0, 1, 1, ccw, 'Coeff')
-entre(0, 2, 1, cmew, 'Moyenne élèves')
-entre(0, 3, 1, cmcw, 'Moyenne classe')
-entre(0, 4, 1, cvmw, 'Validation module')
-entre(0, 5, 1, crw, 'Rattrapage')
-entre(0, 6, 1, cECTSw, 'ECTS')
 
-# separation
-separation(1, 0, 1, cmw)
-separation(1, 1, 1, ccw)
-separation(1, 2, 1, cmew)
-separation(1, 3, 1, cmcw)
-separation(1, 4, 1, cvmw)
-separation(1, 5, 1, crw)
-separation(1, 6, 1, cECTSw)
 
 # Ouverture et lecture du fichier XML
 r_bulletins = "bulletin.xml"
 #w_bulletins = "bulletin.xml"
 def xml_read():
-    tree = et.parse(r_bulletins)
-    root = tree.getroot()
-    print(root.find('eleve/nom').text)
-    #for elem in root:
-    #    for subelem in elem:
-    #        print(subelem.text)
+    # Création et remplissage des différentes catégories
+    entre(0, 0, 1, cmw, 'Matières')
+    entre(0, 1, 1, ccw, 'Coeff')
+    entre(0, 2, 1, cmew, 'Moyenne élèves')
+    entre(0, 3, 1, cmcw, 'Moyenne classe')
+    entre(0, 4, 1, cvmw, 'Validation module')
+    entre(0, 5, 1, crw, 'Rattrapage')
+    entre(0, 6, 1, cECTSw, 'ECTS')
 
+    # separation
+    separation(1, 0, 1, cmw)
+    separation(1, 1, 1, ccw)
+    separation(1, 2, 1, cmew)
+    separation(1, 3, 1, cmcw)
+    separation(1, 4, 1, cvmw)
+    separation(1, 5, 1, crw)
+    separation(1, 6, 1, cECTSw)
 
-# Création et remplissage des champs
-part = str(xml_read())
-entre(2, 0, 1, cmw, part)
+    row = 2
+    tree = ET.ElementTree(ET.fromstring(requests.get("").text)).getroot()
+    for eleve in tree.findall('eleve'):
+        row+=1
+        nom = eleve.find('nom').text
+        prenom = eleve.find('prenom').text
+        nom_module = eleve.get('module')
+        moy_module = eleve.get('moyenne_module')
+        validation_module = eleve.get('validation')
+        ETCS = eleve.get('ECTS')
+        entre(row,0,1,cmw,nom_module)
 
+xml_read()
 root.mainloop()
