327642edf69efb600278d56b5070cd355b09e12b
eaglelamlol <jjacque10@gmail.com>
2020-03-12 11:48:18 +0100

    trombi_vf

---
+++ com/ecetech/b2/gui/test/img_trombi/trombi.py
@@ -0,0 +1,40 @@
+from Tkinter import * 
+from functools import partial
+from lxml import etree 
+
+
+
+root = Tk()
+
+
+
+
+button = []
+photo = []
+a=0
+b=0
+c=0
+
+
+mytree = etree.parse('img_trombi.xml')
+myroot = mytree.getroot()
+
+
+for x in myroot.findall ('personne') :
+	
+	nom=x.find('nom').text
+	img=x.find('image').text
+	photo.insert(c,PhotoImage(file=img))
+	button.insert(c,Button(root,text=nom, image=photo[c] , height=180, width=150,compound="top"))
+	
+	if a>5 :
+		b=b+1
+		a=0
+		
+	button[c].grid (row=b, column=a)
+	a=a+1
+	c=c+1
+
+	
+
+root.mainloop()
